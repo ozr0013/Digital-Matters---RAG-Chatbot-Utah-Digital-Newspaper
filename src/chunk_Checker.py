3ac43450269986ad31f28ca3f702a50c779e158e
Omar Rizwan <omarrizwan7@gmail.com>
2025-10-15 10:38:35 -0600

    Chuncking Success. Faiss vectorization in process.

--- src/chunk_Checker.py
+++ src/chunk_Checker.py
@@ -1,18 +1,3 @@
 import pandas as pd
-import glob
-
-# Path to your chunked folder
-path = r"D:\UDN_Project\chunked\*.csv"
-
-total_chunks = 0
-files = glob.glob(path)
-
-for f in files:
-    try:
-        df = pd.read_csv(f)
-        total_chunks += len(df)
-        print(f"{f} → {len(df)} chunks")
-    except Exception as e:
-        print(f"⚠️ Skipped {f}: {e}")
-
-print(f"\n✅ Total actual chunks across all files: {total_chunks:,}")
+df = pd.read_csv(r"D:\UDN_Project\chunked\udn_chunks_part0.csv")
+print(df.head())
